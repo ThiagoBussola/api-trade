1ecbf61c2082fd137bee298aa9eceeed10283e89
Thiago <thiagobussola@hotmail.com>
2021-08-19 06:07:50 -0300

    configurando ambiente de testes

--- src/cep/cep.controller.spec.ts
+++ src/cep/cep.controller.spec.ts
@@ -1,11 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CepController } from './cep.controller';
+import { CepService } from './cep.service';
 
 describe('CepController', () => {
   let controller: CepController;
+  let service: CepService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers: [CepService],
       controllers: [CepController],
     }).compile();
 
